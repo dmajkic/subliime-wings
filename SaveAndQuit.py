2c5a3a1e901170ebc9175977a22542a86968d3a2
Dusan D. Majkic <dmajkic@lovor.net>
2014-05-29 19:42:34 +0200

    Consistent naming

--- SaveAndQuit.py
+++ SaveAndQuit.py
@@ -1,6 +1,6 @@
 import sublime, sublime_plugin
 
-class SaveAndQuitCommand(sublime_plugin.WindowCommand):
+class ExitSaveAllCommand(sublime_plugin.WindowCommand):
     def run(self):
         self.window.run_command("save_all")
         self.window.run_command("exit")
